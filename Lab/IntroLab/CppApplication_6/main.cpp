88e0a02c92adda3769cdc52f1da7003c4a9e5ca7
FC2724211 <fabriciocarrillo007@gmail.com>
2020-08-27 19:19:50 -0700

    Signed-off-by: FC2724211 <fabriciocarrillo007@gmail.com>

---
+++ Lab/IntroLab/CppApplication_6/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Fabricio Carrillo
+ * Created on 8/27/2020
+ * Purpose:   intro lab
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variable Data Types and Constants
+    
+    
+    //couting hello world
+    cout<<"Hello World"<<endl; 
+    //Exit stage right!
+    return 0;
+}
